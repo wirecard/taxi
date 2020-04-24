c542ea60b6901640a8383340089b4a24eccf1156
Lukas Deutz <lukas.deutz@wirecard.com>
2020-04-24 16:27:57 +0200

    Cleanup Config

--- src/taxi/config.rb
+++ src/taxi/config.rb
@@ -9,7 +9,7 @@ module Taxi
   class Config
     include Singleton
 
-    attr_reader :aws_config, :sftp_config, :aws_credentials
+    attr_reader :aws_config, :sftp_config
 
     # Outputs currently loaded config.
     def print
@@ -17,43 +17,51 @@ module Taxi
       ap @sftp_config
       puts '+ AWS Config'.yellow
       ap @aws_config
+
+      puts '= AWS Settings (updated)'.yellow
+      ap Aws.config
     end
 
     def list_buckets
       puts '* AWS Buckets'.yellow
       s3 = aws_s3_client
+      ap s3
       ap s3.list_buckets
     end
 
     def aws_assume_role
+      tags = ['client TAXI', 'repository wirecard/taxi', 'team tecodc']
+      tags.map! do |entry|
+        key, value = entry.split(' ')
+        { key: key, value: value }
+      end
+
+      ###
       # Create Role Credentials with AssumeRole ARN
+      ###
+      # @aws_sts_client.assume_role(
+      #   role_arn: @aws_config.role_assume,
+      #   role_session_name: 'github://wirecard/taxi',
+      #   duration_seconds: 1200,
+      #   tags: tags
+      # )
       Aws::AssumeRoleCredentials.new(
         client: @aws_sts_client,
         role_arn: @aws_config.role_assume,
         role_session_name: 'github://wirecard/taxi',
         duration_seconds: 1200,
-        tags: [
-          {
-            key: 'client',
-            value: 'TAXI'
-          },
-          {
-            key: 'repository',
-            value: 'wirecard/taxi'
-          },
-          {
-            key: 'team',
-            value:'tecdoc'
-          }
-        ],
-        endpoint: @aws_config.endpoint_url
+        tags: tags
       )
     end
 
     def aws_s3_client
-      role_response = aws_assume_role
-      # Use AssumeRole Credentials to create S3 Client
-      Aws::S3::Client.new(credentials: role_response.credentials)
+      role_credentials = aws_assume_role
+      s3 = Aws::S3::Client.new(
+        credentials: role_credentials,
+        disable_host_prefix_injection: true
+      )
+      ap s3
+      s3
     end
 
     private
