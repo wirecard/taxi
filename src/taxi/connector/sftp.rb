7bd5f3d6e723512b2b9686c855f7eb776502120f
Herbert Knapp <herbert.knapp@wirecard.com>
2020-04-27 13:36:26 +0200

    Add file_get_data, file_put_data, file_upload, file_download [wip]

--- src/taxi/connector/sftp.rb
+++ src/taxi/connector/sftp.rb
@@ -3,16 +3,46 @@ require 'net/sftp'
 module Taxi
   class SFTP
     include Singleton
+
+    def file_download(src, dest)
+      File.write(dest, file_get_data(src))
+    end
+
+    def file_upload(src, dest)
+      file_put_data(dest, File.read(src))
+    end
+
+    def file_exists?(path)
+      dirname = File.dirname(path)
+      filename = File.basename(path)
+      return list_dir(dirname).include? filename
+    end
+
     def list_dir(path = '/')
       @sftp.dir.foreach(path) do |element|
         pp element
       end
     end
 
-    def file_exists?(path)
-      dirname = File.dirname(path)
-      filename = File.basename(path)
-      list_dir(dirname).include? filename
+    def file_get_data(path)
+      data = @sftp.download!(path)
+      return data unless data.nil?
+      if file_exists? path
+        puts "file #{path} exists but cannot be downloaded"
+      else
+        puts "file #{path} does not exist"
+      end
+    end
+
+    def file_put_data(path, data)
+      if file_exists?(path)
+        puts "file #{path} exists. skipping"
+      else
+        @sftp.file.open(path, 'w') do |file|
+          file.puts data
+        end
+       return file_exists?(path)
+      end
     end
 
     private
